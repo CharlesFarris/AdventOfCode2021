9da2f67fc13f323ba1bfe672f50faeafbfa6a88e
Charles Farris <charlesfarris71@gmail.com>
2021-12-16 19:20:36 -0700

    Implement part 1

--- src/day15.ts
+++ src/day15.ts
@@ -1,9 +1,24 @@
 // AoC Day 15 Challenge
 
+import { Map,mapFromLines } from "./map";
+
 export { dayFifteenPartOne, dayFifteenPartTwo };
 
+const lines: string[] = [
+    "1163751742",
+    "1381373672",
+    "2136511328",
+    "3694931569",
+    "7463417111",
+    "1319128137",
+    "1359912421",
+    "3125421639",
+    "1293138521",
+    "2311944581"
+];
+
 function dayFifteenPartOne(): void {
-    // todo
+    const map: Map = mapFromLines(lines);
 }
 
 function dayFifteenPartTwo(): void {
