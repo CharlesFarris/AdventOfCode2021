beebda2930773a33704c8a87317dd8aa9fec9586
Charles Farris <charlesfarris71@gmail.com>
2021-12-18 21:30:57 -0700

    Add stubs

---
+++ src/day18.ts
@@ -0,0 +1,11 @@
+// AoC Day 18 Challenge
+
+export { dayEighteenPartOne, dayEighteenPartTwo };
+
+function dayEighteenPartOne(): void {
+    // todo
+}
+
+function dayEighteenPartTwo(): void {
+    // todo
+}
