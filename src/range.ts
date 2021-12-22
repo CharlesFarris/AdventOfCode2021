f9fbf1c72f958f06b8f3fcb4b5d3009797618a08
Charles Farris <charlesfarris71@gmail.com>
2021-12-22 16:51:53 -0700

    Implement part 1

--- src/range.ts
+++ src/range.ts
@@ -1,4 +1,4 @@
-export { EmptyRange, Range };
+export { EmptyRange, intersection, IntersectionResult, Range, union };
 
 class Range {
     constructor(start: number, end: number) {
@@ -24,3 +24,18 @@ class Range {
 }
 
 const EmptyRange: Range = new Range(0, 0);
+
+function union(left: Range, right: Range): Range {
+    return new Range(Math.min(left.start, right.start), Math.max(left.end, right.end));
+}
+
+class IntersectionResult {
+    constructor(readonly status: boolean, readonly range: Range) {}
+}
+
+function intersection(left: Range, right: Range): IntersectionResult {
+    if (right.start > left.end || left.start > right.end) {
+        return new IntersectionResult(false, EmptyRange);
+    }
+    return new IntersectionResult(true, new Range(Math.max(left.start, right.start), Math.min(left.end, right.end)));
+}
