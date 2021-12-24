edbe7dd1b5ea011647dd0e2e6ab04478204d44b5
Charles Farris <charlesfarris71@gmail.com>
2021-12-24 10:01:38 -0700

    Add stubs

---
+++ src/day24.ts
@@ -0,0 +1,11 @@
+// AoC Day 24 Challenge
+
+export { dayTwentyFourPartOne, dayTwentyFourPartTwo };
+
+function dayTwentyFourPartOne(): void {
+    // todo
+}
+
+function dayTwentyFourPartTwo(): void {
+    // todo
+}
