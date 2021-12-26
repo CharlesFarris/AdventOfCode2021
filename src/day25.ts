ffdcd126993880de6652c6017ff74455e1a6aced
Charles Farris <charlesfarris71@gmail.com>
2021-12-26 01:30:31 -0700

    Add stubs

---
+++ src/day25.ts
@@ -0,0 +1,11 @@
+// AoC Challenge Day 25
+
+export { dayTwentyFivePartOne, dayTwentyFivePartTwo };
+
+function dayTwentyFivePartOne(): void {
+    // todo
+}
+
+function dayTwentyFivePartTwo(): void {
+    // todo
+}
